ea533957d35ebcab51adc295277a37f2e558bbf1
Anderson Aguiar de Souza <31575796+AndersonS001@users.noreply.github.com>
2019-02-16 19:16:24 -0200

    Add files via upload

---
+++ Omicron/src/Omicron.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Omicron {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		char entrada[] = new char[256];
+		char saida[][] = new char[1024][32];
+		
+		System.out.println("Digite em que a informação que sera inserida");
+		entrada = sc.nextLine().toCharArray();
+		
+		System.out.println("Digite a linha em que a informação que sera inserida");
+		int linha = sc.nextInt();
+				
+		System.out.println("Digite a coluna em que a informação que sera inserida");
+		int coluna = sc.nextInt();		
+
+		for(int j = 0; j < 1024; j++)
+			for(int i = 0; i < 32; i++)
+				saida[j][i] = ' ';
+			
+		int ent = 0;
+		for(int i=coluna; i < entrada.length; i++)
+		{
+			saida[linha][i] = entrada[ent];
+			ent++;
+		}
+		
+		Arquivo.Manipulacao(saida);
+	}
+}
